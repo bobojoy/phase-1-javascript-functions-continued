e6113a66a36c880a974bad8b46fc800742a6e308
Joy Wambui <joy.wambui@student.moringaschool.com>
2024-07-12 09:10:12 +0300

    complete

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 // code your solution here
-function saturdayFun (activity = 'roller-skate'){
-    return `This Saturday, I want to ${activity}!`;
-}
+function saturdayFun(activity = 'roller-skate'){
+      return `This Saturday, I want to ${activity}!`;
+ }
 
-function mondayWork (activity = 'go to the office'){
-    return `This Monday, I will ${activity}.`
-}
+ let mondayWork = function(activity = 'go to the office'){
+        return `This Monday, I will ${activity}.`;
+ }
 
-function wrapAdjective (para1 = "*") {
-    return function (para2 ="special"){
-        return `You are ${para1}${para2}${para1}!`;
-    }
-}
+ let wrapAdjective = function (style = "*") {
+   return function (adjective = "special") {
+     return `You are ${style}${adjective}${style}!`;
+   };
+ };
+     
+
+
+ 
